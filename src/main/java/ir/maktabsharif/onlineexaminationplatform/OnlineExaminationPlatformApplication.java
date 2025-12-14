5d7d249034faa3245fca8ec184a096aea56208b4
Ali-f774 <alifaraji774@gmail.com>
2025-12-14 17:23:23 +0330

    add initial files

---
+++ src/main/java/ir/maktabsharif/onlineexaminationplatform/OnlineExaminationPlatformApplication.java
@@ -0,0 +1,13 @@
+package ir.maktabsharif.onlineexaminationplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExaminationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExaminationPlatformApplication.class, args);
+    }
+
+}
