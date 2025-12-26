5294aec321911c1f9fd53bfc8492e4742806b941
Ali-f774 <alifaraji774@gmail.com>
2025-12-26 15:41:48 +0330

    Switch to Keycloak

--- src/main/java/ir/maktabsharif/onlineexaminationplatform/model/User.java
+++ src/main/java/ir/maktabsharif/onlineexaminationplatform/model/User.java
@@ -1,5 +1,6 @@
 package ir.maktabsharif.onlineexaminationplatform.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
